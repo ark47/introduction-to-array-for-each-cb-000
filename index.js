85df4b9fd5aaea4d5bd8de091c05a58eb98d8153
Chris Nunez <cenunez47@gmail.com>
2017-11-07 03:42:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ function doToElementsInArray(array, callback) {
 
 // Add your changeCompletely() function here:
 
+<<<<<<< HEAD
 function changeCompletely(element, index, array) {
   array[index] = Math.floor(Math.random() * 100 + 2).toString() + ` ${array[index]}s!!!`
 }
+=======
+function changeCompletely() {}
+>>>>>>> 0bf15b7b23bad20e9d873d31ff43eafe08ddc850
